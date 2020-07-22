b1d68aacc19995713d2afc633ad6ffcb0ea467c5
gustavo <contato@gustavohalm.com>
2020-07-21 21:13:00 -0300

    modeling entities

--- gameCatalog/settings.py
+++ gameCatalog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '86oq3r@75*jn38o@x=_*nico@q#7&r$tmt1u$2@%963dyx=zkw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'core'
 ]
 
 MIDDLEWARE = [
